88c2920a6311a6a4f2f4adbc45f5f8176db37599
kjkjkk <113284611+kjkjkk@users.noreply.github.com>
2025-09-02 02:52:28 +0000

    Feature: added update and delete functionality

--- src/routes/notes.route.js
+++ src/routes/notes.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 
-import { createNote, viewNote } from "../controller/notes.controller.js";
+import { createNote, viewNote, updateNote, deleteNote } from "../controller/notes.controller.js";
 
 const router = express.Router();
 
 router.post("/create", createNote);
-router.get("/view/:id", createNote);
+router.get("/view/:id", viewNote);
+router.get("/update/:id", updateNote);
+router.get("/delete/:id", deleteNote);
 
 export default router;
